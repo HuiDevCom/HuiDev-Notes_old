1e14373320e4511f776885da64b3f9b1a061967b
HuiDevCom <fenghui@huidev.com>
2026-07-20 09:31:29 +0800

    feat: 更新关于页面的名称，简化为“关于”

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -163,7 +163,7 @@ export const LinkPresets: Record<string, NavBarLink> = {
 		pageKey: "guestbook",
 	},
 	About: {
-		name: "关于我",
+		name: "关于",
 		url: "/about/",
 		icon: "material-symbols:person",
 	},
